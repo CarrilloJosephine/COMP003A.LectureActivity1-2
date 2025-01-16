46db071ac1e66feb106a3ee880884dce4ed62da5
CarrilloJosephine <josephinecarrillo@my.whccd.edu>
2025-01-15 20:55:35 -0800

    add 10

--- COMP003A.LectureActivity1/Program.cs
+++ COMP003A.LectureActivity1/Program.cs
@@ -1,5 +1,11 @@
-﻿//namespace level 
- namespace COMP003A.LectureActivity1
+﻿/*
+    Author: Josephine Carrillo
+    Course: COMP-003A
+    Purpose: Basic console app
+   */
+//namespace level 
+
+namespace COMP003A.LectureActivity1
 { 
     internal class Program
     {
@@ -7,7 +13,16 @@
     // main level
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Hello, World!");
+            Console.WriteLine("2. hi my name is Josephine Carrillo");
+            Console.WriteLine("3. im majoring in both computer and software engineering");
+            Console.WriteLine("4. i enjoy traveling");
+            Console.WriteLine("5. One of my goals is to work for google");
+            Console.WriteLine("6. im the only girl in 5 siblings");
+            Console.WriteLine("7. Im 20 years old");
+            Console.WriteLine("8. I drive a bmw");
+            Console.WriteLine("9. i plan on buying a motorcyle by 2026");
+            Console.WriteLine("10. I like taking pictures of my dog");
         }
     }
 }
